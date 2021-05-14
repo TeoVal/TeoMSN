50ae1f76a85b8efb4fc70d185d24af573e5321fd
TeoVal <valeanv.teodora@gmail.com>
2021-05-14 15:54:08 +0200

    Add a Message class for later use

---
+++ MessageClient/MessageClient/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MessageClient
+{
+    public class Message
+    {
+        public string Content { get; private set; }
+        public string Sender { get; set; }
+
+        public Message(string content, string sender)
+        {
+            this.Content = content;
+            this.Sender = sender;
+        }
+    }
+}
