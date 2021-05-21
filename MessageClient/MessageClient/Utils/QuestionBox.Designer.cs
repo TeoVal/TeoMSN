1d3736fcb841d090a72fc0115c19978ab6e4b17c
TeoVal <valeanv.teodora@gmail.com>
2021-05-21 11:58:33 +0200

    WIP: working on question box

---
+++ MessageClient/MessageClient/Utils/QuestionBox.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace MessageClient.Utils
+{
+    partial class QuestionBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.answerBox = new System.Windows.Forms.TextBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // answerBox
+            // 
+            this.answerBox.Location = new System.Drawing.Point(66, 57);
+            this.answerBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.answerBox.Name = "answerBox";
+            this.answerBox.PlaceholderText = "Type here...";
+            this.answerBox.Size = new System.Drawing.Size(180, 23);
+            this.answerBox.TabIndex = 1;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(195, 102);
+            this.okButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(51, 25);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(66, 102);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(51, 25);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // questionLabel
+            // 
+            this.questionLabel.Location = new System.Drawing.Point(12, 9);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(288, 34);
+            this.questionLabel.TabIndex = 0;
+            this.questionLabel.Text = "This will be replaced by the actual question";
+            this.questionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // QuestionBox
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(312, 138);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.answerBox);
+            this.Controls.Add(this.questionLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "QuestionBox";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Question Title";
+            this.Load += new System.EventHandler(this.QuestionBox_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox answerBox;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label questionLabel;
+    }
+}
---
+++ MessageClient/MessageClient/Utils/QuestionBox.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MessageClient.Utils
+{
+    public partial class QuestionBox : Form
+    {
+        private string Answer { get; set; } = null;
+
+        private string Question { get; set; }
+
+        private string FormTitle { get; set; }
+
+        public QuestionBox()
+        {
+            InitializeComponent();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            Answer = answerBox.Text;
+            this.Close();
+        }
+
+        private void QuestionBox_Load(object sender, EventArgs e)
+        {
+            questionLabel.Text = Question;
+            this.Text = FormTitle;
+        }
+
+        public static string Ask(string question, string title)
+        {
+            string answer = null;
+            
+
+
+
+
+
+            return answer;
+        }
+    }
+}
